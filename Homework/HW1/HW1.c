b95496d32920312e3974a328064ab1bb4abd4ac0
Kaider-GitHub <paul40807@gmail.com>
2019-06-13 00:26:18 +0800

    finish homework1

--- Homework/HW1/HW1.c
+++ Homework/HW1/HW1.c
@@ -36,7 +36,8 @@ void genOp1(int i, char c) {
   printf("# t%d=%c\n", i, c);
   // t1=3 轉成 @3; D=A; @t1; M=D
   printf("@%c\n", c);
-  printf("D=A\n");
+  if (isdigit(c)) printf("D=A\n");
+  else            printf("D=M\n");
   printf("@t%d\n", i);
   printf("M=D\n");
 }
@@ -55,33 +56,43 @@ void genOp2(int i, int i1, char op, int i2) {
 // F =  Number | '(' E ')'
 int F() {
   int f;
-  char c = ch();
-  if (isdigit(c)) {
-    next(); // skip c
-    f = nextTemp();
-    genOp1(f, c);
-  } else if (c=='(') { // '(' E ')'
+  if (ch()=='(') {
     next();
     f = E();
     assert(ch()==')');
     next();
-  } else {
-    error("F = (E) | Number fail!");
-  }
+  } else { 
+    f = nextTemp();
+    char c=next();
+    genOp1(f, c);
+  } 
   return f; 
 }
 
 // E = F ([+-] F)*
 int E() {
-  int i1 = F();
+  int i1 = T();
   while (isNext("+-")) {
     char op=next();
-    int i2 = F();
+    int i2 = T();
     int i = nextTemp();
     genOp2(i, i1, op, i2);
     i1 = i;
   }
   return i1;
+}
+
+// T = F ([*/]F)*
+int T() {
+  int f1 = F();
+  while (isNext("*/")) {
+    char op=next();
+    int f2 = F();
+    int f = nextTemp();
+    genOp2(f, f1, op, f2);
+    f1 = f;
+  }
+  return f1;
 }
 
 void parse(char *str) {
@@ -92,6 +103,7 @@ void parse(char *str) {
 int main(int argc, char * argv[]) {
   printf("=== EBNF Grammar =====\n");
   printf("E=F ([+-] F)*\n");
+  printf("T=F ([*/] F)*\n");
   printf("F=Number | '(' E ')'\n");
   printf("==== parse:%s ========\n", argv[1]);
   parse(argv[1]);
